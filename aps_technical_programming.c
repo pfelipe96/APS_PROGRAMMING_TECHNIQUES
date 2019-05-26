2b912872619acae70c5ea413c93352d4ef6a38f4
Paulo <p.felipe@msn.com>
2019-05-26 16:07:28 -0300

    Change method bubble for mergeSort

--- aps_technical_programming.c
+++ aps_technical_programming.c
@@ -5,7 +5,6 @@ const int length = 4;
 
 void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[length], int length);
 float averagePlayers(float it[length], int length);
-void bubbleSort(float it[length], int n);
 float medianPlayers(float heightPlayers[length]);
 
 int main(){
@@ -100,27 +99,88 @@ float averagePlayers(float it[length], int length){
 
 //Calcular a mediana dos jogadores
 float medianPlayers(float heightPlayers[length]){
-    bubbleSort(heightPlayers, length);
+    mergeSort(heightPlayers, length);
 
-    float varOne = heightPlayers[1];
-    float varSecond = heightPlayers[2];
+    if(length % 2 = 0){
+
+    }else{
+        
+    }
 
     return (varOne + varSecond) / 2;
 }
 
 //Ordernar o vetor por causa da função mediana.
-void bubbleSort(float it[length], int n) {
-    int k, j, aux;
-
-    for (k = 1; k < n; k++) {
-        for (j = 0; j < n - 1; j++) {
-            if (it[j] > it[j + 1]) {
-                aux = it[j];
-                it[j] = it[j + 1];
-                it[j + 1] = aux;
-            }
-        }
-    }
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) { 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) { 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
 }
 
 
