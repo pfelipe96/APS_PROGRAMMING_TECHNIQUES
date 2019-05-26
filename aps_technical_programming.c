0904bbf12ceca9063f13d90a55035025673d806d
Paulo <p.felipe@msn.com>
2019-05-26 17:38:41 -0300

    Added method for sum standardDeviation and higherAndLowerHeight of players

--- aps_technical_programming.c
+++ aps_technical_programming.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int length = 4;
+const int length = 2;
 
-void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[length], int length);
-float averagePlayers(float it[length], int length);
+void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[length]);
+float averagePlayers(float it[length]);
 float medianPlayers(float heightPlayers[length]);
+void mergeSort(float arr[], int l, int r);
+void merge(float arr[], int l, int m, int r);
+void higherAndLowerHeight(char aboutPlayers[length][length], float heightPlayers[length]);
+float standardDeviationPlayers(float heightPlayers[length]);
 
 int main(){
     char fullNamesPlayers[length][length];
@@ -13,22 +17,33 @@ int main(){
     char registerPlayers[length][length];
     int isOpen = 1;
     int option = 0;
+    int count = 0;
+
+    printf("Grupo: \n Paulo Felipe Oliveira de Souza RA 20898988 \n Guilherme Takakuwa 20913698 \n Vitor Cunha Silva 20900163 \n Levi da Costa Silva 20755873\n\n");
 
     //Get names on players 
     for(int i = 0; i < length; i++){
         printf("%s %d \n", "Insira o nome do jogador ", i);
-        scanf("%s", &fullNamesPlayers[i]);
-        printf("%s \n", "Insira altura do jogador em cm:");
-        scanf("%f",&heightPlayers[i]);
-        // printf("%s %d \n", "Insira o RA do jogador:", i);
-        // scanf("%s", registerPlayers[i]);
+        scanf("%s", &fullNamesPlayers[i]);  
+    
+        float inputHeight = 0;
+
+        do{
+            printf("%s \n", "Insira altura do jogador em cm (acima de 0 cm):");
+            scanf("%f",&inputHeight);
+            if(inputHeight > 0){
+                heightPlayers[i] = inputHeight;
+            }
+        }while(inputHeight < 0);
     }
 
     printf("\n");
 
+    mergeSort(heightPlayers, 0, (length - 1));
+
     //Do e o switch serve para blb
     do{
-        printf("Escolha a opção:\n1 - Mostra os dados do jogadores:\n2 - Média de altura dos jogadores:\n5 - Mediana de altura dos jogadores:\n6 - Sair\n");
+        printf("Escolha a opção:\n1 - Mostra os dados do jogadores:\n2 - Média de altura dos jogadores:\n3 - Desvio Padrão das alturas:\n4 - Maior e Menor altura:\n5 - Mediana de altura dos jogadores:\n6 - Sair\n");
 
         scanf("%d", &option);
 
@@ -37,7 +52,7 @@ int main(){
         switch (option){
             case 1:
                 if(fullNamesPlayers[0] != NULL){
-                    dataAboutPlayers(fullNamesPlayers, heightPlayers, length);
+                    dataAboutPlayers(fullNamesPlayers, heightPlayers);
                 }else{
                     printf("Não tem nenhum dado de jogadores registrado.");
                 }
@@ -45,7 +60,23 @@ int main(){
                 break;
             case 2:
                 if(fullNamesPlayers[0] != NULL){
-                    printf("%s %f\n", "Média da altura dos jogadores é: ", averagePlayers(heightPlayers, length));
+                    printf("%s %f\n", "Média da altura dos jogadores é: ", averagePlayers(heightPlayers));
+                }else{
+                    printf("Não tem nenhum dado de jogadores registrado.");
+                }
+                printf("\n");
+                break;
+            case 3:
+                if(fullNamesPlayers[0] != NULL){
+                    printf("%s %f\n", "Desvio padrão dos jogadores é: ", standardDeviationPlayers(heightPlayers));
+                }else{
+                    printf("Não tem nenhum dado de jogadores registrado.");
+                }
+                printf("\n");
+                break;
+            case 4:
+                if(fullNamesPlayers[0] != NULL){
+                    higherAndLowerHeight(fullNamesPlayers, heightPlayers);
                 }else{
                     printf("Não tem nenhum dado de jogadores registrado.");
                 }
@@ -60,7 +91,7 @@ int main(){
                 printf("\n");
                 break;
             case 6:
-                printf("Até mais tarde!");
+                printf("Volte sempre!");
                 printf("\n");
                 exit(0);
                 break;
@@ -73,7 +104,7 @@ int main(){
 }
 
 //Recupera os dados dos jogadores e mostra o nome, altura e registro.
-void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[length], int length){
+void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[length]){
     for(int i = 0; i < length; i++){
         printf("%s \n", "-----------------------");
         printf("%s | %f cm\n", aboutPlayers[i], heightPlayers[i]);
@@ -81,7 +112,7 @@ void dataAboutPlayers(char aboutPlayers[length][length], float heightPlayers[len
 }
 
 //Calcular a média da altura dos jogadores
-float averagePlayers(float it[length], int length){
+float averagePlayers(float it[length]){
     float sumAll = 0;
 
     for(int i = 0; i < length; i++){
@@ -91,30 +122,47 @@ float averagePlayers(float it[length], int length){
     return (sumAll / length);
 }
 
-// double standardDeviationPlayers(){
+float standardDeviationPlayers(float heightPlayers[length]){
+    float sumHeightPow, sumHeight = 0;
+    float average = averagePlayers(heightPlayers);
 
-// }
+    for(int i = 0; i < length; i++){
+        //Picking up only one player at a time
+        float heightPlayer = heightPlayers[i];
+        //Calculating player height by square
+        sumHeightPow += (heightPlayer*heightPlayer);
+        //Adding height of all players
+        sumHeight += heightPlayer;
+    }
 
-// double higherAndLowerHeight(int time, int isHighe){}
+    return (sumHeightPow/sumHeight) - (average*average);
+}
+
+void higherAndLowerHeight(char aboutPlayers[length][length], float heightPlayers[length]){
+    printf("Menor jogador: %s | %f cm\n", aboutPlayers[0], heightPlayers[0]);
+    printf("Maior jogador: %s | %f cm\n", aboutPlayers[(length - 1)], heightPlayers[(length - 1)]);
+}
 
 //Calcular a mediana dos jogadores
 float medianPlayers(float heightPlayers[length]){
-    mergeSort(heightPlayers, length);
+    int half = (length / 2);
 
-    if(length % 2 = 0){
-
+    if(length % 2 == 0){
+        float varOne = heightPlayers[half];
+        float varSecond = heightPlayers[half - 1];
+        return (varOne + varSecond) / 2;
     }else{
-        
+        return heightPlayers[(half - 1)];
     }
-
-    return (varOne + varSecond) / 2;
 }
 
-//Ordernar o vetor por causa da função mediana.
+//Ordernar o vetor por causa da função mediana e maior e menor altura.
+
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) { 
+// Method get on @https://www.geeksforgeeks.org/merge-sort/
+void merge(float arr[], int l, int m, int r) { 
     int i, j, k; 
     int n1 = m - l + 1; 
     int n2 =  r - m; 
@@ -168,7 +216,7 @@ void merge(int arr[], int l, int m, int r) {
   
 /* l is for left index and r is right index of the 
    sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r) { 
+void mergeSort(float arr[], int l, int r) { 
     if (l < r) 
     { 
         // Same as (l+r)/2, but avoids overflow for 
